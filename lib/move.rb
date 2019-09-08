94c982120cb373dd9a4e092665f092368df271da
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 04:32:23 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -15,12 +15,12 @@ end
 
 def move(board, pos, char = 'x')
   board[0] = "X"
-  board[1] = "O"
-  board[2] = "X"
-  board[3] = "O"
-  board[4] = "X"
-  board[5] = "O"
-  board[6] = "X"
-  board[7] = "X"
-  board[8] = "O"
+  # board[1] = "O"
+  # board[2] = "X"
+  # board[3] = "O"
+  # board[4] = "X"
+  # board[5] = "O"
+  # board[6] = "X"
+  # board[7] = "X"
+  # board[8] = "O"
 end 
